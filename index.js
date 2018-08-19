c63a38c64ccf1b581f89af10fa4c4e9e4922beef
lihonglei <lihonglei@autohome.com.cn>
2018-08-19 13:36:55 +0800

    根据数组列表，循环拼接DOM结构，每4个span，外围包裹一个DIV节点。

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * @description 根据数组列表，循环拼接DOM结构，每4个span，外围包裹一个DIV节点。
+ */
+
+let list = []; // 接口返回的数据
+let anchorArray = ['<div class="swiper-slide clearfix">'];
+const step = 4;
+for (let [index, elem] of list.entries()) {
+    let num = index + 1;
+    anchorArray.push("<span></span>");
+
+    if (num % step == 0) {
+        if (num >= list.length) {
+            anchorArray.push('</div>');
+        } else {
+            anchorArray.push('</div><div class="swiper-slide clearfix">');
+        }
+    } else {
+        if (num >= list.length) {
+            anchorArray.push('</div>');
+        }
+    }
+}
